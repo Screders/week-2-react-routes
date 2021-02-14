bc2228a5fd291c3d2158c89092c379c856acdd99
Ilya <ilya.gelasimov1@gmail.com>
2021-02-14 23:34:42 +0300

    updated

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -8,12 +8,12 @@ const Dashboard = () => {
       <Head title="Hello" />
       <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
         Dashboard
-        <div>
-          <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
-        </div>
-        <div>
-          <Link to="/dashboard/main">Go To Main</Link>
-        </div>
+      </div>
+      <div>
+        <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
+      </div>
+      <div>
+        <Link to="/dashboard/main">Go To Main</Link>
       </div>
     </div>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -8,12 +8,12 @@ const Main = () => {
       <Head title="Hello" />
       <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
         Main
-        <div>
-          <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
-        </div>
-        <div>
-          <Link to="/dashboard">Go To Root</Link>
-        </div>
+      </div>
+      <div>
+        <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
+      </div>
+      <div>
+        <Link to="/dashboard">Go To Root</Link>
       </div>
     </div>
   )
