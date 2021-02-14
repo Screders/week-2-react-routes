b1f87e772d09554d46cb3fbfd616c132387a2a8e
Ilya <ilya.gelasimov1@gmail.com>
2021-02-14 22:17:01 +0300

    part 2 is done

---
+++ client/components/dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Head from './head'
+
+const Dashboard = () => {   
+  return (
+    <div>
+      <Head title="Hello" />
+      <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        Dashboard
+        <div>
+          <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
+        </div>
+        <div>
+          <Link to="/dashboard/main">Go To Main</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Dashboard.propTypes = {}
+
+export default Dashboard
---
+++ client/components/main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Head from './head'
+
+const Main = () => {
+  return (
+    <div>
+      <Head title="Hello" />
+      <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        Main
+        <div>
+          <Link to="/dashboard/profile/fe505ea1-786b-4658-b0b4-6fce6879830e">Go To Profile</Link>
+        </div>
+        <div>
+          <Link to="/dashboard">Go To Root</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Main.propTypes = {}
+
+export default Main
